f09d9b7094d69ad085f1dcdde1d7a98941d52fbc
OwoJama <jamestinuola007@gmail.com>
2023-09-04 16:10:24 +0100

    version 1.0

---
+++ todo_list.py
@@ -0,0 +1,10 @@
+£ write a simple python code that stores todo list
+
+tasks = []
+
+def add_task(task):
+	tasks.append(task)
+	task = input("Today is a great day")
+	print(f"Task: '{tasks}' added succesfully"}
+	
+def add_task(task)
